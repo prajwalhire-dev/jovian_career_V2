3a308ae2fef3ca05698343982ee90f5decbf2432
prajwal hiremath <>
2023-02-24 14:31:59 +0000

    Added the Flask Project

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+#here app is just an object of the class Flask
+#by below we created a flask application
+app = Flask(__name__) # in python __name__ is used for how a particular script was invoked, like if the script is invoked by python app.py then it has '__main__' as o/p & if u use other the ans will be different
+@app.route('/')
+def hello_world():
+  return 'Hello World'
+
+if __name__ == '__main__':
+  app.run(host = '0.0.0.0', debug = True)
